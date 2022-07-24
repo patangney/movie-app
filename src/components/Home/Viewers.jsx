a67de9200717c2ab7ed917328a844e0b3d4b2cec
Patrick Tangney <ptangney@outlook.com>
2022-07-24 16:44:51 +0100

    setup viewers

--- src/components/Home/Viewers.jsx
+++ src/components/Home/Viewers.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Viewers = () => {
   return (
-    <div>Viewers</div>
+    <div className='viewers'>
+      <div className='viewers__container'>
+        <div className='viewers__container__item'>
+          <img src='/assets/images/viewers-disney.png' alt='' />
+        </div>
+        <div className='viewers__container__item'>
+          <img src='/assets/images/viewers-marvel.png' alt='' />
+        </div>
+        <div className='viewers__container__item'>
+          <img src='/assets/images/viewers-national.png' alt='' />
+        </div>
+        <div className='viewers__container__item'>
+          <img src='/assets/images/viewers-pixar.png' alt='' />
+        </div>
+        <div className='viewers__container__item'>
+          <img src='/assets/images/viewers-starwars.png' alt='' />
+        </div>
+      </div>
+    </div>
   )
 }
 
